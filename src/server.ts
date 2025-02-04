85f8535a5a3f42d651edde6780eb1bfd813df765
WesleyBSa <batistawesley948@gmail.com>
2025-02-04 08:27:10 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import { router } from './routes'
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(router)
+
+app.use((err: Error, req: Request, res:Response, next: NextFunction)=> {
+    if(err instanceof Error) {
+        //Se for a instância do tipo error
+        res.status(400).json({
+            error: err.message
+        })
+    }
+
+    res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+})
+
+app.listen(5555, ()=> console.log('Servidor Online!!!'))
