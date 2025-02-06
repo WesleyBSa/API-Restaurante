ae66a1d3f3a6c4f5a35e82ce29cb06e467cb7ad5
WesleyBSa <batistawesley948@gmail.com>
2025-02-06 12:39:29 -0300

    middleware configurado e testado | add @types/user_id

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface PayLoad{
+    sub: string;
+}
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+
+    const authToken = req.headers.authorization;
+
+    if(!authToken){
+        res.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try{
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as PayLoad;
+
+        //Recuperar o ID do token e colocar dentro de uma variavel user_id dentro do request.
+        req.user_id = sub;
+
+        return next();
+
+    }catch(err){
+        res.status(401).end();
+    }
+
+}
