f3b91c8fdec5a530dba1544ed48b9e99766fc854
WesleyBSa <batistawesley948@gmail.com>
2025-02-07 16:25:47 -0300

    Criado filtro de produtos por categoria

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ import { CreateGategoryController } from './controllers/category/CreateGategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import uploadConfig from './config/multer';
@@ -34,6 +35,8 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //--------ROTAS PRODUCT--------
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 
 export { router };
