4fae1a55f0a0fd323d2e1d5b3f7130f2be9340e4
WesleyBSa <batistawesley948@gmail.com>
2025-02-10 11:22:36 -0300

    Add Rotas de Pedidos

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,9 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import uploadConfig from './config/multer';
 
@@ -19,24 +22,25 @@ const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"));
 
+
 //--------ROTAS USER--------
 router.post('/users', new CreateUserController().handle)
-
 router.post('/session', new AuthUserController().handle)
-
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 
 //--------ROTAS CATEGORY--------
 router.post('/category', isAuthenticated, new CreateGategoryController().handle)
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 
 //--------ROTAS PRODUCT--------
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
-
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
+
+//--------ROTAS ORDER--------
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router };
